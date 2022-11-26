35d47ca1b47c7beda561329004cb642bfa08eff5
kyle-kingsbury <kyle.kingsbury@shopbottools.com>
2022-11-26 15:41:47 -0500

    Fix function name

--- test_runMacros.py
+++ test_runMacros.py
@@ -4,21 +4,18 @@ import time
 
 macro_number = 0 
 
-def test_runMacro_two():
+def test_runMacro_two_hundred_one():
     macro_number = 201 
     r = requests.post(f'{config.API_URL}/macros/{macro_number}/run')
     assert r.status_code == 200
-    time.sleep(10)
 
 def test_runMacro_two():
     macro_number = 2 
     r = requests.post(f'{config.API_URL}/macros/{macro_number}/run')
     assert r.status_code == 200
-    time.sleep(15)
 
 def test_runMacro_three():
     macro_number = 3 
     r = requests.post(f'{config.API_URL}/macros/{macro_number}/run')
     assert r.status_code == 200
-    time.sleep(15)
 
