61491278d38887bb815772928c9df2d77cc5cf81
kyle-kingsbury <kyle.kingsbury@shopbottools.com>
2022-11-26 17:12:27 -0500

    Seperating one test

---
+++ test_runMacro_two_hundred_one.py
@@ -0,0 +1,10 @@
+import requests
+from config import config
+import time
+
+macro_number = 0 
+
+def test_runMacro_two_hundred_one():
+    macro_number = 201 
+    r = requests.post(f'{config.API_URL}/macros/{macro_number}/run')
+    assert r.status_code == 200
--- test_runMacros.py
+++ test_runMacros.py
@@ -4,11 +4,6 @@ import time
 
 macro_number = 0 
 
-def test_runMacro_two_hundred_one():
-    macro_number = 201 
-    r = requests.post(f'{config.API_URL}/macros/{macro_number}/run')
-    assert r.status_code == 200
-
 def test_runMacro_two():
     macro_number = 2 
     r = requests.post(f'{config.API_URL}/macros/{macro_number}/run')
