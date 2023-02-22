bcf6543b1fed56fe55300c45ee1e7ddf643a7cee
kyle-kingsbury <kyle.kingsbury@shopbottools.com>
2023-02-22 10:52:23 -0500

    Can submit a job, cleaned up

--- test_submit.py
+++ test_submit.py
@@ -1,17 +1,8 @@
-import requests
-import time
 import json
-import threading
-from config import config
-from message_monitor import MessageMonitor
 import http.client
 import io, codecs, mimetypes, sys, uuid
-import webbrowser
 
 TIMEOUT = 3
-global mm 
-mm = MessageMonitor()
-mm.clear_all_state()
 conn = http.client.HTTPConnection('10.24.1.71', 80, timeout=TIMEOUT)
 
 class MultipartFormdataEncoder(object):
@@ -62,10 +53,9 @@ class MultipartFormdataEncoder(object):
             body.write(chunk)
         return self.content_type, body.getvalue()
 
-
-def submitJob(results):
+def submitJob():
     
-    filename = "job.nc"
+    filename = "job.sbp"
     name = "test_name"
     description = "test_description"
 
@@ -84,21 +74,15 @@ def submitJob(results):
     json_payload = json.dumps(metadata)
     conn.request("POST", "/job", json_payload, headers)
     response = conn.getresponse()
-    #r = requests.post(f'{config.API_URL}/job', json_payload, headers)
     response_text = response.read().decode('utf-8')
     print(response_text)
     response_data = json.loads(response_text)['data']
     print(response_data)
-    #if r.status_code != 200:
-    #    results["code"] = False
-    #    results["msg"] = "bad http code"
-    #    return
 
     # Second request
-    codes = "mx, 10 mx, 0"
+    codes = "mx, 10\nmx, 0"
     content_type, body = MultipartFormdataEncoder().encode([('key', response_data['key']), ('index',0)], [('file', filename, io.BytesIO(codes.encode('utf-8')))])
     headers = {"Content-type":content_type, "Accept":"text/plain"}
-    #r = requests.post(f'{config.API_URL}/job', body, headers)
     conn.request("POST", "/job", body, headers)
     response = conn.getresponse()
     print(response)
@@ -106,38 +90,7 @@ def submitJob(results):
     response_data = json.loads(response_text)
     print(response_data)
     conn.close()
-    #if r.status_code != 200:
-    #    results["code"] = False
-    #    results["msg"] = "bad http code"
-    #    return
 
-    # Check if test passed
-    results["code"] = True
-    results["msg"] = "success"
-    return 
-
-def thread_for_mm(args):
-    mm.run()
-
-# test function
-def test_submitJob():
-    # setting things up so test can run
-    messageMonitorThread = threading.Thread(target=thread_for_mm, args=(1,), daemon=True)
-    results = {"code":False, "msg":""}
-    testThread = threading.Thread(target=submitJob, args=(results,))
-
-    # test sequence 
-    messageMonitorThread.start() 
-    time.sleep(1) # time for the MessageMonitor to get up and running
-    testThread.start()
-    testThread.join() #waitig for the test to return
-
-    #reporting results
-    # debug (i'm sure there is pytest way to turn this on and off)
-    #print(results)
-    assert(results["code"] == True)
- 
 if __name__ == "__main__":
-    print(config.API_URL)
-    test_submitJob()
+    submitJob()
 
