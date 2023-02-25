2e37c540e6be802f51a913944059aa751b848195
kyle-kingsbury <kyle.kingsbury@shopbottools.com>
2023-02-25 12:01:38 -0500

    Move files around

--- test_getters.py
+++ test_getters.py
@@ -9,15 +9,19 @@ def test_getConfig():
 def test_getJob():
     r = requests.get(f'{config.API_URL}/job')
     assert r.status_code == 200
+    return r.json()
 
 def test_getJobQueue():
     r = requests.get(f'{config.API_URL}/job')
     assert r.status_code == 200
+    return r.json()
 
 def test_getStatus():
     r = requests.get(f'{config.API_URL}/status/')
     assert r.status_code == 200
+    return r.json()
 
 def test_getVersion():
     r = requests.get(f'{config.API_URL}/version/')
     assert r.status_code == 200
+    return r.json()
