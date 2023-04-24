ba1f702ceb519cd07348530553e63be7cb7afe5f
kyle-kingsbury <kyle.kingsbury@shopbottools.com>
2023-04-24 10:40:04 -0400

    Fix run macro requests.

--- macro.py
+++ macro.py
@@ -5,17 +5,17 @@ class Macro:
     def __init__(self):
         self.initialized = 1
 
-    # TODO def update_macro
+    # TODO def update_macro()
 
     def delete(self, macro_id):
         r = requests.delete(f'{config.API_URL}/macros/{macro_id}', timeout=config.TIMEOUT)
         print(r.text)
 
     def run_macro(self, macro_id):
-        r = requests.post(f'{config.API_URL}/macros/{macro_id}', timeout=config.TIMEOUT)
+        r = requests.post(f'{config.API_URL}/macros/{macro_id}/run', timeout=config.TIMEOUT)
 
     def get_macros(self):
-        r = requests.get(f'{config.API_URL}/macros', timeout=config.TIMEOUT)
+        r = requests.get(f'{config.API_URL}/macros/', timeout=config.TIMEOUT)
         assert r.status_code == 200
         return r.json()
 
