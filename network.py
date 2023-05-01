e1f8623dc06ad3332101b26f7264cee204dc97ed
root <root@MSI.localdomain>
2023-05-01 11:08:20 -0400

    Network class

--- network.py
+++ network.py
@@ -1,24 +1,62 @@
-#class network
-    # TODO def connect_to_wifi()
+import requests
+from config import config
 
-    # TODO def disconnect_from_wifi()
+class Network:
+    def __init__(self):
+        self.initialized = 1    
 
-    # TODO def forget_wifi()
+    def connect_to_wifi(self, ssid, password):
+        data = { 'ssid': ssid, 'password': password }
+        r = requests.post(f'{config.API_URL}/network/wifi/connect', data, timeout = config.TIMEOUT)
+        assert r.status_code == 200
 
-    # TODO def enable_wifi()
+    def disconnect_from_wifi(self):
+        r = requests.post(f'{config.API_URL}/network/wifi/disconnect', timeout = config.TIMEOUT)
+        assert r.status_code == 200
 
-    # TODO def disable_wifi()
+    def forget_wifi(self):
+        r = requests.post(f'{config.API_URL}/network/wifi/forget', timeout = config.TIMEOUT)
+        assert r.status_code == 200
 
-    # TODO def enable_hotspot()
+    def enable_wifi(self):
+        data = {'enabled': True}
+        r = requests.post(f'{config.API_URL}/network/wifi/state', data, timeout = config.TIMEOUT)
+        assert r.status_code == 200
 
-    # TODO def disable_hotspot()
+    def disable_wifi(self):
+        data = {'enabled': False}
+        r = requests.post(f'{config.API_URL}/network/wifi/state', data, timeout = config.TIMEOUT)
+        assert r.status_code == 200
 
-    # TODO def get_network_identity()
+    def enable_hotspot(self):
+        data = {'enabled': True}
+        r = requests.post(f'{config.API_URL}/network/hotspot/state', data, timeout = config.TIMEOUT)
+        assert r.status_code == 200
 
-    # TODO def set_network_identity()
+    def disable_hotspot(self):
+        data = {'enabled': False}
+        r = requests.post(f'{config.API_URL}/network/hotspot/state', data, timeout = config.TIMEOUT)
+        assert r.status_code == 200
 
-    # TODO def get_is_online()
+    def get_network_identity(self):
+        r = requests.get(f'{config.API_URL}/network/identity', timeout = config.TIMEOUT)
+        assert r.status_code == 200
 
-    # TODO def get_wifi_networks()
+    def set_network_identity(self, identity):
+        r = requests.post(f'{config.API_URL}/network/identity', data = identity, timeout = config.TIMEOUT)
+        assert r.status_code == 200
 
-    # TODO def get_wifi_network_history()
+    def get_is_online(self):
+        r = requests.get(f'{config.API_URL}/network/online', timeout = config.TIMEOUT)
+        assert r.status_code == 200
+    
+    def get_wifi_networks(self):
+        r = requests.get(f'{config.API_URL}/network/wifi/scan', timeout = config.TIMEOUT)
+        assert r.status_code == 200
+
+    def get_wifi_network_history(self):
+        r = requests.get(f'{config.API_URL}/network/wifi/scan', timeout = config.TIMEOUT)
+        assert r.status_code == 200
+
+if __name__ == "__main__":
+    network = Network()
--- updater.py
+++ updater.py
@@ -1,7 +1,19 @@
-# class updater
+import requests
+from config import config
 
-    # def set_updater_config():
+class Updater:
+    def __init__(self):
+        self.initialized = 1
 
-    # def submit_update():
+    def get_config(self):
+        r = requests.get(f'{config.API_URL}/updater/config', timeout = config.TIMEOUT)
+        assert r.status_code == 200
+        return r.json()
 
-    # def get_updater_status()
+    def set_config(self, config_data):
+        r = requests.get(f'{config.API_URL}/updater/config', data = config_data, timeout = config.TIMEOUT)
+        assert r.status_code == 200
+        return r.json()
+
+if __name__ == "__main__":
+    updater = Updater()
